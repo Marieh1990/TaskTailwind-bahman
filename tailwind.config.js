40d17d76eec9b653d69771185a18d0f3e896b91a
Marieh1990 <eshghimaria@gmail.com>
2025-01-31 13:32:44 +0330

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+      extend: {
+        fontFamily:{
+          sans :["Vazirmatn"]
+        }
+      },
+    },
+    plugins: [],
+  }
+  
